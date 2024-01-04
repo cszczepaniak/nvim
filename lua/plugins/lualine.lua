386b3bc2d164a5e55815618ea2dcbfac67e5bb03
Connor Szczepaniak <cszczepaniak@gmail.com>
2024-01-03 20:28:58 -0600

    show relative path of file

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,7 +4,15 @@ return {
         require('lualine').setup({
             options = {
                 theme = 'dracula',
-            }
+            },
+            sections = {
+                lualine_c = {
+                    {
+                        'filename',
+                        path = 1,
+                    },
+                },
+            },
         })
     end,
 }
