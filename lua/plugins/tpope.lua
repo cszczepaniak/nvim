ef85497fa1339c425ab01c47986f14748299c600
Connor Szczepaniak <cszczepaniak@gmail.com>
2024-01-08 12:56:28 -0600

    add vim unimpaired

--- lua/plugins/tpope.lua
+++ lua/plugins/tpope.lua
@@ -1,18 +1,21 @@
 return {
-    {
-        "tpope/vim-surround",
-    },
-    {
-        "tpope/vim-commentary",
-    },
-    {
-        "tpope/vim-rhubarb",
-    },
-    {
-        "tpope/vim-fugitive",
-        config = function()
-            vim.keymap.set("n", "<leader>gc", ":Git commit<CR>", {})
-            vim.keymap.set("n", "<leader>gca", ":Git commit -a<CR>", {})
-        end,
-    },
+	{
+		"tpope/vim-surround",
+	},
+	{
+		"tpope/vim-commentary",
+	},
+	{
+		"tpope/vim-rhubarb",
+	},
+	{
+		"tpope/vim-unimpaired",
+	},
+	{
+		"tpope/vim-fugitive",
+		config = function()
+			vim.keymap.set("n", "<leader>gc", ":Git commit<CR>", {})
+			vim.keymap.set("n", "<leader>gca", ":Git commit -a<CR>", {})
+		end,
+	},
 }
