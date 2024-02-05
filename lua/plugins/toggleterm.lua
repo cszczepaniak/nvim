301cb8d1801719099f5d00c94cc849b8fb11f06c
Connor Szczepaniak <cszczepaniak@gmail.com>
2024-02-04 20:08:26 -0600

    setup toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,6 +2,7 @@ return {
     {
         "akinsho/toggleterm.nvim",
         config = function()
+            require("toggleterm").setup()
             local terminal = require("toggleterm.terminal").Terminal
             local lazygit = terminal:new({ direction = "float", cmd = "lazygit", hidden = true })
 
