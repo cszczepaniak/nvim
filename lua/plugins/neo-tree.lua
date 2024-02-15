615236a7fab7c210c15941c1291199a82e5dd474
Connor Szczepaniak <cszczepaniak@gmail.com>
2024-02-15 10:53:30 -0600

    show hidden

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,22 +1,31 @@
 local is_open = false
 
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons",
-        "MunifTanjim/nui.nvim",
-    },
-    config = function()
-        vim.keymap.set("n", "<C-n>", function()
-            if is_open then
-                vim.cmd(":Neotree close")
-                is_open = false
-            else
-                vim.cmd(":Neotree filesystem reveal left")
-                is_open = true
-            end
-        end, {})
-    end,
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					hide_dotfiles = false,
+					hide_gitignored = true,
+				},
+			},
+		})
+		vim.keymap.set("n", "<C-n>", function()
+			if is_open then
+				vim.cmd(":Neotree close")
+				is_open = false
+			else
+				vim.cmd(":Neotree filesystem reveal left")
+				is_open = true
+			end
+		end, {})
+	end,
 }
