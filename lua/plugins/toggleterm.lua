d75108ef36cc1b73041c9ee895c5df11e0085db0
Connor Szczepaniak <cszczepaniak@gmail.com>
2024-02-15 10:52:32 -0600

    add keymap for termselect in toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,15 +1,21 @@
 return {
-    {
-        "akinsho/toggleterm.nvim",
-        config = function()
-            require("toggleterm").setup()
-            local terminal = require("toggleterm.terminal").Terminal
-            local lazygit = terminal:new({ direction = "float", cmd = "lazygit", hidden = true })
+	{
+		"akinsho/toggleterm.nvim",
+		config = function()
+			require("toggleterm").setup()
+			local terminal = require("toggleterm.terminal").Terminal
+			local lazygit = terminal:new({ direction = "float", cmd = "lazygit", hidden = true })
 
-            vim.api.nvim_create_user_command("LazyGitToggle", function()
-                lazygit:toggle()
-            end, {})
-            vim.api.nvim_set_keymap("n", "<leader>G", "<cmd>LazyGitToggle<CR>", { noremap = true, silent = true })
-        end,
-    },
+			vim.api.nvim_create_user_command("LazyGitToggle", function()
+				lazygit:toggle()
+			end, {})
+			vim.api.nvim_set_keymap("n", "<leader>G", "<cmd>LazyGitToggle<CR>", { noremap = true, silent = true })
+			vim.api.nvim_set_keymap("n", "<leader>ft", "<cmd>TermSelect<CR>", { noremap = true, silent = true })
+			vim.api.nvim_create_user_command("TermNew", function()
+				local t = terminal:new({ direction = "vertical", cmd = "ls", close_on_exit = false })
+				t:resize(50)
+				t:toggle()
+			end, {})
+		end,
+	},
 }
