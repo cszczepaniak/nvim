0ed5fb56b3eee7789c31e5f13940046f2f99afb5
Connor Szczepaniak <cszczepaniak@gmail.com>
2024-02-14 21:34:52 -0600

    format?

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,59 +1,80 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter-context",
-    },
-    {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                textobjects = {
-                    select = {
-                        enable = true,
-                        -- Automatically jump forward to textobj, similar to targets.vim
-                        lookahead = true,
-                        keymaps = {
-                            -- You can use the capture groups defined in textobjects.scm
-                            ["aS"] = "@statement.outer",
-                            ["at"] = "@class.outer",
-                            ["it"] = "@class.inner",
-                            ["af"] = "@function.outer",
-                            ["if"] = "@function.inner",
-                            ["aF"] = "@call.outer",
-                            ["iF"] = "@call.inner",
-                            ["aa"] = "@parameter.outer",
-                            ["ia"] = "@parameter.inner",
-                            ["al"] = "@loop.outer",
-                            ["il"] = "@loop.inner",
-                            ["iC"] = "@conditional.inner",
-                            ["aC"] = "@conditional.outer",
-                        },
-                    },
-                },
-            })
-        end,
-    },
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-            local config = require("nvim-treesitter.configs")
-            config.setup({
-                ensure_installed = {
-                    "lua",
-                    "vimdoc",
-                    "go",
-                    "gomod",
-                    "gosum",
-                    "gowork",
-                    "zig",
-                    "rust",
-                    "c",
-                    "javascript",
-                    "typescript",
-                },
-                highlight = { enable = true },
-                indent = { enable = true },
-            })
-        end,
-    },
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				textobjects = {
+					select = {
+						enable = true,
+						-- Automatically jump forward to textobj, similar to targets.vim
+						lookahead = true,
+						keymaps = {
+							-- You can use the capture groups defined in textobjects.scm
+							["aS"] = "@statement.outer",
+							["at"] = "@class.outer",
+							["it"] = "@class.inner",
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["aF"] = "@call.outer",
+							["iF"] = "@call.inner",
+							["aa"] = "@parameter.outer",
+							["ia"] = "@parameter.inner",
+							["al"] = "@loop.outer",
+							["il"] = "@loop.inner",
+							["iC"] = "@conditional.inner",
+							["aC"] = "@conditional.outer",
+						},
+					},
+					swap = {
+						enable = true,
+						swap_next = {
+							["<leader>a"] = "@parameter.inner",
+						},
+						swap_previous = {
+							["<leader>A"] = "@parameter.inner",
+						},
+					},
+					move = {
+						enable = true,
+						set_jumps = true,
+						goto_next_start = {
+							["]m"] = "@function.outer",
+							["]a"] = "@parameter.inner",
+						},
+						goto_previous_start = {
+							["[m"] = "@function.outer",
+							["[a"] = "@parameter.inner",
+						},
+					},
+				},
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				ensure_installed = {
+					"lua",
+					"vimdoc",
+					"go",
+					"gomod",
+					"gosum",
+					"gowork",
+					"zig",
+					"rust",
+					"c",
+					"javascript",
+					"typescript",
+				},
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 }
